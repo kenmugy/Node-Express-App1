f41ea70c530d90e7908d4db5869ddb7d6d7d40dd
ken <kenmugy@gmail.com>
2019-08-25 15:25:39 +0300

    created static files for the app and set up middleware for the stylesheets and javascript using materialize-css and jquery

--- server.js
+++ server.js
@@ -8,6 +8,19 @@ const app = express();
 const port = process.env.PORT || '4000';
 
 app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  '/css',
+  express.static(path.join(__dirname, '/node_modules/materialize-css/dist/css'))
+);
+app.use(
+  '/js',
+  express.static(path.join(__dirname, '/node_modules/jquery/dist'))
+);
+app.use(
+  '/js',
+  express.static(path.join(__dirname, '/node_modules/materialize-css/dist/js'))
+);
 
 app.get('/', (req, res) => {
   res.send('hello world');
